6812a77db320d773b31c6728f40eca0f00eae959
Тихий Никита <dark_angel382@mail.ru>
2022-04-06 10:53:50 +0300

    TZ файнал

--- ТЗ/Application.cs
+++ ТЗ/Application.cs
@@ -8,7 +8,7 @@ namespace ТЗ
 {
     internal class Application
     {
-        static string str = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+ "\\Фигуры\\Triangles.json";
+        static string str;
 
         public static async void  Start()
         {
@@ -18,20 +18,6 @@ namespace ТЗ
             List<Circle> circles = new List<Circle>();
             List<Polygon> polygons = new List<Polygon>();
             List<Figure> figures = new List<Figure>();
-            triangles = JsonSerializer.Deserialize<List<Triangle>>(File.ReadAllText(str));
-            str = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Quads.json";
-            quads = JsonSerializer.Deserialize<List<Quad>>(File.ReadAllText(str));
-            str = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Rectangles.json";
-            rectangles = JsonSerializer.Deserialize<List<Rectangle>>(File.ReadAllText(str));
-            str = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Circles.json";
-            circles = JsonSerializer.Deserialize<List<Circle>>(File.ReadAllText(str));
-            str = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Polygons.json";
-            polygons = JsonSerializer.Deserialize<List<Polygon>>(File.ReadAllText(str));
-            figures.AddRange(triangles);
-            figures.AddRange(quads);
-            figures.AddRange(rectangles);
-            figures.AddRange(circles);
-            figures.AddRange(polygons);
             bool flag = true;
             while (flag)
             {
@@ -71,72 +57,80 @@ namespace ТЗ
                         break;
 
                     case 0:
-                        if (figures.Count() != 0)
-                        {
-                            try
-                            {
-                                var list = figures.Distinct().ToList();
-                                foreach (var fig in list)
-                                {
-                                    fig.GetPerimetr();
-                                    fig.GetSquare();
-                                    if (fig.Title == "Треугольник")
-                                        triangles.Add((Triangle)fig);
-                                    if (fig.Title == "Квадрат")
-                                        quads.Add((Quad)fig);
-                                    if (fig.Title == "Прямоугольник")
-                                        rectangles.Add((Rectangle)fig);
-                                    if (fig.Title == "Окружность")
-                                        circles.Add((Circle)fig);
-                                    if (fig.Title == "Многоугольник")
-                                        polygons.Add((Polygon)fig);
-                                }
-                                triangles = triangles.Distinct().ToList();
-                                quads = quads.Distinct().ToList();
-                                rectangles = rectangles.Distinct().ToList();
-                                circles = circles.Distinct().ToList();
-                                polygons = polygons.Distinct().ToList();
-                                string strT = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Triangles.json";
-                                using (FileStream fs = new FileStream(strT, FileMode.OpenOrCreate))
-                                {
-                                    JsonSerializer.SerializeAsync(fs,triangles);
-                                
-                                }
-                                string strQ = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Quads.json";
-                                using (FileStream fs = new FileStream(strQ, FileMode.OpenOrCreate))
-                                {
-                                    JsonSerializer.SerializeAsync(fs, quads);
-
-                                }
-                                string strR = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Rectangles.json";
-                                using (FileStream fs = new FileStream(strR, FileMode.OpenOrCreate))
-                                {
-                                    JsonSerializer.SerializeAsync(fs, rectangles);
-
-                                }
-                                string strC = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Circles.json";
-                                using (FileStream fs = new FileStream(strC, FileMode.OpenOrCreate))
-                                {
-                                    JsonSerializer.SerializeAsync(fs, circles);
-
-                                }
-                                string strP = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Фигуры\\Polygons.json";
-                                using (FileStream fs = new FileStream(strP, FileMode.OpenOrCreate))
-                                {
-                                    JsonSerializer.SerializeAsync(fs, polygons);
-
-                                }
-                                flag = false;
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine("Ошибка. " + e.Message);
-                            }
-                        }
-                        else
                             flag = false;
                         break;
 
+                    case 8:
+                        Console.WriteLine("Введите путь к файлам с фигурами: ");
+                        str = Console.ReadLine();
+                        triangles = JsonSerializer.Deserialize<List<Triangle>>(File.ReadAllText(str + "\\Triangles.json"));
+                        quads = JsonSerializer.Deserialize<List<Quad>>(File.ReadAllText(str + "\\Quads.json"));
+                        rectangles = JsonSerializer.Deserialize<List<Rectangle>>(File.ReadAllText(str + "\\Rectangles.json"));
+                        circles = JsonSerializer.Deserialize<List<Circle>>(File.ReadAllText(str + "\\Circles.json"));
+                        polygons = JsonSerializer.Deserialize<List<Polygon>>(File.ReadAllText(str + "\\Polygons.json"));
+                        figures.AddRange(triangles);
+                        figures.AddRange(quads);
+                        figures.AddRange(rectangles);
+                        figures.AddRange(circles);
+                        figures.AddRange(polygons);
+                        break;
+                    
+                    case 9:
+                        try
+                        {
+                            var list = figures.Distinct().ToList();
+                            foreach (var fig in list)
+                            {
+                                fig.GetPerimetr();
+                                fig.GetSquare();
+                                if (fig.Title == "Треугольник")
+                                    triangles.Add((Triangle)fig);
+                                if (fig.Title == "Квадрат")
+                                    quads.Add((Quad)fig);
+                                if (fig.Title == "Прямоугольник")
+                                    rectangles.Add((Rectangle)fig);
+                                if (fig.Title == "Окружность")
+                                    circles.Add((Circle)fig);
+                                if (fig.Title == "Многоугольник")
+                                    polygons.Add((Polygon)fig);
+                            }
+                            triangles = triangles.Distinct().ToList();
+                            quads = quads.Distinct().ToList();
+                            rectangles = rectangles.Distinct().ToList();
+                            circles = circles.Distinct().ToList();
+                            polygons = polygons.Distinct().ToList();
+                            using (FileStream fs = new FileStream(str + "\\Triangles.json", FileMode.OpenOrCreate))
+                            {
+                                JsonSerializer.SerializeAsync(fs, triangles);
+
+                            }
+                            using (FileStream fs = new FileStream(str + "\\Quads.json", FileMode.OpenOrCreate))
+                            {
+                                JsonSerializer.SerializeAsync(fs, quads);
+
+                            }
+                            using (FileStream fs = new FileStream(str + "\\Rectangles.json", FileMode.OpenOrCreate))
+                            {
+                                JsonSerializer.SerializeAsync(fs, rectangles);
+
+                            }
+                            using (FileStream fs = new FileStream(str + "\\Circles.json", FileMode.OpenOrCreate))
+                            {
+                                JsonSerializer.SerializeAsync(fs, circles);
+
+                            }
+                            using (FileStream fs = new FileStream(str + "\\Polygons.json", FileMode.OpenOrCreate))
+                            {
+                                JsonSerializer.SerializeAsync(fs, polygons);
+
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Ошибка. " + e.Message);
+                        }
+                break; 
+                    
                     case 6:
                         if (figures.Count() != 0)
                         {
